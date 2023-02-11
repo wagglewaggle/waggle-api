3a3ed6fd70d1eebc4b2833e90218e506e2595b36
jiho5993 <jiho5993@naver.com>
2023-02-11 19:13:27 +0900

    style: 불필요한 service & 코드 제거

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { NaverService } from './services/naver.service';
 
 @Module({
-  providers: [AuthService, NaverService],
+  providers: [NaverService],
   controllers: [AuthController],
 })
 export class AuthModule {}
