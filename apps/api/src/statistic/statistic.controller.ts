11559876e8eb13b238dff836a9bcfc8d3113ebe8
jiho5993 <jiho5993@naver.com>
2023-05-21 15:21:32 +0900

    feat: admin guard 구현

--- apps/api/src/statistic/statistic.controller.ts
+++ apps/api/src/statistic/statistic.controller.ts
@@ -1,8 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { ApiPath } from './statistic.constant';
 import { StatisticService } from './statistic.service';
+import { UserGuard } from '../app/guards/user.guard';
+import { AdminGuard } from '../app/guards/admin.guard';
 
 @Controller(ApiPath.Root)
+@UseGuards(UserGuard, AdminGuard)
 export class StatisticController {
   constructor(private readonly statisticService: StatisticService) {}
 
